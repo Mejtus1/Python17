05696563d53e04bcf725fc32eb5ac8fb93f9de92
Mejtus1 <GitMaster1769@proton.me>
2023-06-21 11:32:54 +0200

    python basics

---
+++ Python1_Basics.py
@@ -0,0 +1,33 @@
+# Python 
+# Python excells in shell scripting task automation and web development 
+# It can create games and work with embedded devices 
+# It is also used in Data Analysis and Machine learning 
+#You can use REPLIT to code and run programs in a web browser 
+
+#Rock paper scissors game - VARIABLES IN PXTHON 
+name = "Beau" #we created variable and assigned it value Beau
+age = 39 #number
+#variables can be capitale leaters, strings, underscore starts but IT CANNOT start with a number 
+#there cannot be variables of python keywords (for, if, whiole, import)
+
+#Expressions in python 
+#any code that returns a value 
+1 + 1 
+
+#Statements in python 
+#is an operation on a value, it is doing something to a value 
+name2 = "Victoria" #statement
+print(name2) #statement2
+
+#COMMNETS 
+# we use hash marks for comments 
+
+#INDENTATION 
+#in python indentation matters 
+name3 = "Hey"
+print(name3)
+    print(name3) #here we have indentation error because of tap 
+
+
+#DATA TYPES IN PYTHON ..................................
+54:27
