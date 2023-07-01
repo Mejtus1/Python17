27757adeea2541fe6031d25a8eca7e5496ef49e7
Mejtus1 <GitMaster1769@proton.me>
2023-07-01 11:33:53 +0200

    python data types

--- AutomateCybersecurityTasks.py
+++ AutomateCybersecurityTasks.py
@@ -4,3 +4,37 @@
 #print - outputs a specified object to a screen 
 print("Hello, Python")
 
+
+#--------------------------------------------------------------------------#
+#--------------------------------------------------------------------------#
+#DATA TYPES IN PYTHON 
+
+#Strings 
+#Data consisting of an ordered sequence of characters
+#letters, symbols, spaces or numbers (numbers cannot be used in calculations)
+#strings need to be inside quoation marks ""
+print("hello python")
+
+#Float data
+#Data consisting of a number with a decimal point 
+# 2.1 10.1 10.00
+
+#Integer data 
+#data consisting of a number that does not include a decimal point 
+# 1, 48 , 12478514
+
+print(1 + 1)#calcualtion with print 
+
+#boolean data
+#Data that cen only be one of two values - true or false
+print(10<5)
+print(9<12) #Determine boolean values 
+
+#List Data
+#Data structure that consists of a collection of data in sequantial form 
+print(["dtanaka", "mabadi", "aestrada"]) #(like arrays in JS)
+
+
+#--------------------------------------------------------------------------#
+#--------------------------------------------------------------------------#
+# Work with variables in python 
