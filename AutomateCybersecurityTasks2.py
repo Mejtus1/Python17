e6c60eeb4528bce3bbd371e9b87bd340fd87d3ee
Mejtus1 <GitMaster1769@proton.me>
2023-07-17 12:01:37 +0200

    paramethers inside functions

--- AutomateCybersecurityTasks2.py
+++ AutomateCybersecurityTasks2.py
@@ -47,3 +47,27 @@ for i in username_list:
     print(sum_variable)
 
 list_to_string()
+
+
+#--------------------------------------------------------------------------#
+#--------------------------------------------------------------------------#
+#USE paramethers inside a functions 
+#Paramether is an object that is included inside a function in use for that function 
+# -//- are accepted into a function throught a parenthesis after a function name 
+range(start, stop) # start and stop values are range function paramethers
+range(3, 7) #the sequence it generates will run from 3 to 6
+
+#greet employees by name paramether
+def greet_employee(name):
+    print("Welcome! You are logged in", name)
+#name is here a paramether defined in our function, and we want to use it when function is invoked
+
+#Argument is a data brought into a function when it is called
+greet_employee("Vicky") #Vicky is an argument 
+
+
+#more paramethers and arguments inside a functions 
+def greet_employee2(first_name2, last_name2):
+    print("Welcome! You are logged in", first_name2, last_name2)
+
+greet_employee2("Vicky", "Jack")
