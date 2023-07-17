c71565fde7adead6faec52d5d7467f9627662d2e
Mejtus1 <GitMaster1769@proton.me>
2023-07-17 11:16:40 +0200

    functions

---
+++ AutomateCybersecurityTasks2.py
@@ -0,0 +1,11 @@
+#Functions 
+#using for reusing same lines of code 
+#it is a section of code that can be reused in a program 
+
+#def is placed before a functin name to define a function
+def
+#we define a functin 
+def greet_employee():
+    print("Welcome! You are logged in.")
+    #call a function 
+greet_employee()
