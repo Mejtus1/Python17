021a4342e223f05af07035a67d7897b39a0030e7
Mejtus1 <GitMaster1769@proton.me>
2023-07-23 23:48:31 +0200

    automate cybersecurity tasks

---
+++ AutomateCybersecurityTasks4.py
@@ -0,0 +1,13 @@
+#Automate cybersecurity tasks with python 
+#Automating tasks in Python
+#Automation is the use of technology to reduce human and manual effort to perform common and repetitive tasks.
+#Automating cybersecurity-related tasks requires 
+#understanding the following Python components:
+#Variables
+#Conditional statements
+#Iterative statements
+#Functions
+#Techniques for working with strings
+#Techniques for working with lists
+#Working with files in Python
+
