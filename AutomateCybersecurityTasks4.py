54f5131d1504cc804968db59ddb47ff291a15545
Mejtus1 <GitMaster1769@proton.me>
2023-08-07 20:19:46 +0200

    logic errors

--- AutomateCybersecurityTasks4.py
+++ AutomateCybersecurityTasks4.py
@@ -307,6 +307,33 @@ def update_file(import_file, remove_list)
 #Logic error = logic inside code doesnt function properly ( if statement with < 3 condition, someone forgets that it should be <= 3 )
 #Exceptions = (mathematically impossible, accessing index values or variables that doesnt exist)
 
+#Explore debugging techniques
+#Syntax errors 
+#A syntax error is an error that involves invalid usage of a programming language. 
+#Syntax errors occur when there is a mistake with the Python syntax itself. 
+#Commin examples of syntax errors include forgetting a punctuation mark, such as 
+#a closing bracket for a list or a colon after a function header.  
+#When you run code with syntax errors, the output will identify the location of 
+#the error with the line number and a portion of the affected code. It also describes 
+#the error. Syntax errors often begin with the label  "SyntaxError:" . Then, this is 
+#followed by a  description of the error. The description might simply be "invalid syntax" . 
+#Or if you forget a closing parentheses on a function, the description might be 
+#"unexpected EOF while parsing". "EOF" stands for "end of file."  
+message = "You are debugging a syntax error
+print(message)
+#This outputs the message "SyntaxError: EOL while scanning string literal". 
+#"EOL" stands for "end of line". 
+#The error message also indicates that the error happens on the first line. )
+#The error occurred because a quotation mark was missing at the end of the string on the first line. 
+#You can fix it by adding that quotation mark.
+
+#Note: You will sometimes encounter the error label "IndentationError" instead of "SyntaxError". 
+#"IndentationError" is a subclass of "SyntaxError" that occurs when the indentation 
+#used with a line of code is not syntactically correct. 
+
+
+
+
 ##########################################################################
 #Python file algorithm TEST
 
