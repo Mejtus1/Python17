be62753a271184f5706523f5473e3237b47278c1
Mejtus1 <GitMaster1769@proton.me>
2023-09-04 22:55:18 +0200

    expressions and variables

--- IBM_python_basics.py
+++ IBM_python_basics.py
@@ -7,3 +7,21 @@
 
 #Boolean 
 #True or False
+
+##############################################
+#Expressions and Variables
+#Expressions describe a type of operation the computers perform
+#Expressions are operations the python performs
+
+#Methematical expressions 
+print(5+5)
+print(4-4)
+print(4*4)
+print(4/4)
+print(25//6) 
+#python follows mathematical conventions 
+
+#Variables
+my_variable = 1
+print(my_variable)
+#Variable names should be correlated with values or value they represent 
