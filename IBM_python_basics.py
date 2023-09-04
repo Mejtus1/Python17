a37f24cae13c38570f528c0bd94c5c6436491ee2
Mejtus1 <GitMaster1769@proton.me>
2023-09-04 23:54:44 +0200

    Strings and string operations

--- IBM_python_basics.py
+++ IBM_python_basics.py
@@ -8,7 +8,7 @@
 #Boolean 
 #True or False
 
-##############################################
+############################################################################################
 #Expressions and Variables
 #Expressions describe a type of operation the computers perform
 #Expressions are operations the python performs
@@ -25,3 +25,37 @@ print(25//6)
 my_variable = 1
 print(my_variable)
 #Variable names should be correlated with values or value they represent 
+
+############################################################################################
+#String Operations 
+#String is a sequence of characters 
+Name = "Vicky"
+#each Character is represented by a index (starts at 0) 
+#V = 0, i = 1, c = 2, k = 3, y = 4
+
+#we can concatenate strings 
+Name = "Vicky"
+Statement = Name + "is my name"
+print(Statement)
+3* "Vicky" #3 copies of the string 
+
+#\n represents a new line in a string
+print("Hello, my name is \n Vicky") 
+
+#String Methods and functinos 
+#.upper()
+A="Hey there"
+B = A.upper() #value of B is similar to A but all the characters ale upper case
+
+C = "Hey there"
+D = C.replace("Hey","hello") #replaces Hey with hello
+
+
+
+
+
+
+
+############################################################################################
+############################################################################################
+############################################################################################
