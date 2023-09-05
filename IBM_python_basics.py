f0517e4958106f71e4bfec6bbe92d6c0bd37cefc
Mejtus1 <GitMaster1769@proton.me>
2023-09-05 10:00:59 +0200

    loops and else statement

--- IBM_python_basics.py
+++ IBM_python_basics.py
@@ -48,3 +48,48 @@ int(string) #ValueError
 #   >   Greather than
 #   >=  Greater than or equal to 
 
+#Data Structures
+#Touple = an immutable sequence of objects (), (1,2,3), ("Python", 3)
+#List = a mutable sequence of objects [], [1,2,3], ["Hello", "World"]
+#Set = an mutable/immutable unordered set of unique items {1,2,3}  {"World", "Hello"}
+#Dicitonary = a mutable and not ordered collector {},{"name":"Ezio","surname":"Melotti"}
+#           = a key value pair 
+
+#Conditions and Branching
+#If, elif, else
+#To trigger a command, given a certain condition, we can use if, elif, else statements:
+#
+# If = establishes the first condition
+# Elif = is used when many conditions are possible 
+# Else = will execute its command if none of the if and elif conditions were met 
+
+Print("Hello")
+if name == "Antonio":       #Hello!
+    print("It is Antonio")  #it is Antionio
+print("How are you?")       #How are you? 
+
+print("a=",a)
+if a > 0:
+    print("a is positive")
+elif a<0:
+    print("a is negative")
+else:
+    print("a is zero")
+print("This will be printed in any case")
+
+#For loops 
+L = ["Red", "Blue", "Yellow"]
+print(L[0]) #"Red"
+print(L[1]) #"Blue"
+
+N = len(L)
+for i in range(N):
+    print(L[i])
+]
+#While loops 
+L = [1, 0, 6, 8, 4, -3, 7, 5, -1]
+
+i=0
+while (L[i]>=0):
+    print(L[i])
+    i=i+1
